8f5249dc15420b5d3446269861225d9c865e0809
wiji <wiji.permana@gmail.com>
2022-07-09 23:51:49 +0700

    Local storage

--- src/contexts/DataContext.jsx
+++ src/contexts/DataContext.jsx
@@ -8,7 +8,17 @@ export function useData() {
 
 export function DataProvider({ children }) {
   const [quantity, setQuantity] = useState(0);
-  const [cartItems, setCartItems] = useState([]);
+  const [cartItems, setCartItems] = useState(() => {
+    const localItem = localStorage.getItem(
+      "ECOMMERCE_PRODUCT_PAGE_FRONTENDMENTOR"
+    );
+    if (localItem) {
+      const parsedItem = JSON.parse(localItem);
+      return [parsedItem];
+    }
+    console.log(localItem);
+    return [];
+  });
 
   const [isLightboxOpen, setIsLightboxOpen] = useState(false);
   const [isOffcanvasOpen, setIsOffcanvasOpen] = useState(false);
